43a953d638f7868c11c9729653c50f9db10f3637
darlev91 <darlev91@gmail.com>
2019-01-21 10:08:31 +0200

    download backup using stream

--- acapi/resources/tests/test_acquiadata.py
+++ acapi/resources/tests/test_acquiadata.py
@@ -53,7 +53,7 @@ class TestAcquiaData(unittest.TestCase):
     def test_get_404(self, m):
         """Tests a GET request that receives a 404 response."""
 
-        uri = self.base_uri + 'invalid'
+        uri = "{base_uri}invalid".format(base_uri=self.base_uri)
 
         m.register_uri('GET', uri + '.json', status_code=400)
 
@@ -65,17 +65,33 @@ class TestAcquiaData(unittest.TestCase):
 
     def test_get_500s_retry(self, m):
         """Tests retrying a GET request that receives 50x response."""
-        uri = self.base_uri + 'test'
+        uri = "{base_uri}test".format(base_uri=self.base_uri)
 
-        m.register_uri('GET', uri + '.json', status_code=503)
-        m.register_uri('GET', uri + '.json?acapi_retry=1', status_code=504)
-        m.register_uri('GET', uri + '.json?acapi_retry=2', json={})
+        m.register_uri("GET", "{uri}.json".format(uri=uri), status_code=503)
+        m.register_uri(
+            "GET",
+            "{uri}.json?acapi_retry=1".format(uri=uri),
+            status_code=504
+        )
+        m.register_uri(
+            "GET",
+            "{uri}.json?acapi_retry=2".format(uri=uri),
+            json={}
+        )
 
         data = {}
         adata = AcquiaData(uri, None, data)
 
         response = adata.request()
         self.assertIsInstance(response, dict)
+
+    def test_get_response_content(self, m):
+        uri = "{domain}.json".format(domain=self.domain_uri)
+        m.register_uri('GET', uri, status_code=200)
+
+        adata = AcquiaData(self.domain_uri, None, {})
+        response = adata.request(decode_json=False)
+        self.assertIsInstance(response, bytes)
 
 
 if __name__ == '__main__':
